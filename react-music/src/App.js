eec91b0ff2d1065ed3b72a6388b8d33d6db69e86
AaronJiang <aaron.jiang@nzme.co.nz>
2020-04-05 19:10:43 +1200

    Init commit

---
+++ react-music/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IconStyle } from "./assets/iconfont/iconfont";
+import { GlobalStyle } from "./style";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <IconStyle />
+      <i className="iconfont">&#xe62b;</i>
+    </div>
+  );
+}
+
+export default App;
